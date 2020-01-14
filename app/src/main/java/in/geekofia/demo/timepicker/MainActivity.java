c3b5686ee22adfbaec7ed9cf2f7ef7caf56b0474
chankruze <chankruze@gmail.com>
2020-01-14 14:37:48 +0530

    Final Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/in/geekofia/demo/timepicker/MainActivity.java
+++ app/src/main/java/in/geekofia/demo/timepicker/MainActivity.java
@@ -1,14 +1,36 @@
 package in.geekofia.demo.timepicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.tv_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TImePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView textView = findViewById(R.id.tv_time);
+        textView.setText(getResources().getString(R.string.time, hourOfDay, minute));
     }
 }
